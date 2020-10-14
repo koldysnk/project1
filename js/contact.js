0c8a3d8e7a1d2c7d6d5c1de0536b5dd84108d171
Nick Koldys <koldysnk@dukes.jmu.edu>
2020-10-14 12:33:26 -0400

    renamed all images

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,7 @@
 const googleForm = document.getElementById("google-form");
 const customForm = document.getElementsByClassName("form-element");
+const send = document.getElementById("send");
+const openForm = document.getElementById('google-form-button');
 
 document.getElementById('form-type').addEventListener('change', function() {
 
@@ -9,3 +11,11 @@ document.getElementById('form-type').addEventListener('change', function() {
     }
     googleForm.classList.toggle("hidden");
   });
+
+send.addEventListener('click',() => {
+    alert("This form of contact is currently under development. Sorry for your inconvenience.");
+});
+
+openForm.addEventListener('click',()=>{
+    window.open('https://forms.gle/Uj6PD3iNv51tUWUy9');
+});
