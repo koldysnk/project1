3d0c7c07fe2e8318c4d2669313ef6cd56927b663
Nick Koldys <koldysnk@dukes.jmu.edu>
2020-10-15 19:33:05 -0400

    report

--- js/contact.js
+++ js/contact.js
@@ -3,6 +3,8 @@ const customForm = document.getElementsByClassName("form-element");
 const send = document.getElementById("send");
 const openForm = document.getElementById('google-form-button');
 
+
+// for changing the active form
 document.getElementById('form-type').addEventListener('change', function() {
 
     
@@ -10,12 +12,16 @@ document.getElementById('form-type').addEventListener('change', function() {
         customForm[i].classList.toggle("hidden");
     }
     googleForm.classList.toggle("hidden");
-  });
+});
 
+
+//for showing alert when submit is clicked
 send.addEventListener('click',() => {
     alert("This form of contact is currently under development. Sorry for your inconvenience.");
 });
 
+
+//for opening google form
 openForm.addEventListener('click',()=>{
     window.open('https://forms.gle/Uj6PD3iNv51tUWUy9');
 });
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const navigation = document.getElementById('nav');
 const mobileNav = document.getElementById("mobile-nav");
 
+
+// for nav list on mobile 
 mobileNav.addEventListener('click', event =>{
     navigation.classList.toggle('open');
     event.stopPropagation();
